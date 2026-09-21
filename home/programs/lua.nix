24b094f822b4e7a92cbfc068d24ce4a9fa670a92
lPhiNix <152783810+lPhiNix@users.noreply.github.com>
2026-09-21 09:34:31 +0200

    feat(home): add lua

---
+++ home/programs/lua.nix
@@ -0,0 +1,14 @@
+#    _  ___        __
+#   / |/ (_)_ __  / /  __ _____ _
+#  /    / /\ \ / / /__/ // / _ `/
+# /_/|_/_//_\_\ /____/\_,_/\_,_/
+# --------------------------------
+# Lua nix home packages by lPhiNix
+#
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    lua5_4 # Lua 5.4 interpreter and luac compiler
+    lua-language-server # Lua language server (LSP)
+    stylua # Lua formatter
+  ];
+}
